1285df36e5f7488c7dc6bea9fcff63d165d69176
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-18 15:09:38 +0900

    프로젝트 초기화

---
+++ src/test/java/kimch321/spring/semiprojectv7/SemiProjectV7BootApplicationTests.java
@@ -0,0 +1,13 @@
+package kimch321.spring.semiprojectv7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemiProjectV7BootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
