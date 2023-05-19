4a5a461de03ab69c4f7593fe057fcf062b830aa2
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-19 13:18:23 +0900

    Board 테이블 생성 및 insert 준비

--- src/main/java/kimch321/spring/semiprojectv7/model/Board.java
+++ src/main/java/kimch321/spring/semiprojectv7/model/Board.java
@@ -1,14 +1,34 @@
 package kimch321.spring.semiprojectv7.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
 
 @Data
+@Entity
+@Table(name = "BOARD")
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String bno;
     private String title;
     private String userid;
+
+    @Column(insertable = false, updatable = false)
     private String thumbs;
+    @Column(insertable = false, updatable = false)
     private String views;
+
     private String content;
+
+    @CreatedDate
+    @Column(insertable = false, updatable = false)
     private String regdate;
 }
