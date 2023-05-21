a21c4c37c458cc2d85173789a2d67644d3e739a1
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-22 00:22:20 +0900

    우편번호 like 검색 및 페이징 테스트

--- src/main/java/kimch321/spring/semiprojectv7/repository/ZipcodeRepository.java
+++ src/main/java/kimch321/spring/semiprojectv7/repository/ZipcodeRepository.java
@@ -1,17 +1,26 @@
 package kimch321.spring.semiprojectv7.repository;
 
 import kimch321.spring.semiprojectv7.model.Zipcode;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ZipcodeRepository
-        extends JpaRepository<Zipcode, Long> {
-    // 메서드 쿼리: find엔티티명All, find엔티티명By컬럼명
+//public interface ZipcodeRepository extends JpaRepository<Zipcode, Long> {
+
+public interface ZipcodeRepository extends PagingAndSortingRepository<Zipcode, Long> {
+
     @Query("from Zipcode where dong like %:dong%")
     List<Zipcode> findZipcodeByDong(@Param("dong") String dong);
+
+    // 전체 조회 중 페이징
+    Page<Zipcode> findAll(Pageable pageable);
+
+    // 검색 중 페이징
+    Page<Zipcode> findByDongLike(String dong, Pageable pageable);
 
 
 }
--- src/test/java/kimch321/spring/semiprojectv7/ZipcodeTest.java
+++ src/test/java/kimch321/spring/semiprojectv7/ZipcodeTest.java
@@ -6,6 +6,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -24,4 +27,27 @@ public class ZipcodeTest {
         for(Zipcode addr: addrs)
             System.out.println(addr);
     }
+
+    @Test
+    @DisplayName("zipcode page1")
+    public void pageZipcode() {
+
+        Pageable pageable = PageRequest.of(0, 15);  // 1페이지
+        Page<Zipcode> page = zipcodeRepository.findAll(pageable);
+        List<Zipcode> zips = page.getContent();
+
+        System.out.println(zips);
+    }
+
+    @Test
+    @DisplayName("zipcode page2")
+    public void page2Zipcode() {
+        String dong = "%" + "구로" + "%";
+        Pageable pageable = PageRequest.of(0, 15);  // 1페이지
+        Page<Zipcode> page = zipcodeRepository.findByDongLike(dong, pageable);
+        List<Zipcode> zips = page.getContent();
+
+        System.out.println(zips);
+    }
+
 }
