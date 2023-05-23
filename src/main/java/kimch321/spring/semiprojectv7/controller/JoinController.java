4a2525ea3b279f3bd430f4651f1ef32e9d223a06
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-23 23:18:53 +0900

    컨트롤러에 유효성 검사 모델 정의 및 검사 후 처리 구현 (세션방식)

--- src/main/java/kimch321/spring/semiprojectv7/controller/JoinController.java
+++ src/main/java/kimch321/spring/semiprojectv7/controller/JoinController.java
@@ -1,9 +1,12 @@
 package kimch321.spring.semiprojectv7.controller;
 
+import kimch321.spring.semiprojectv7.model.Checkme;
 import kimch321.spring.semiprojectv7.model.Member;
 import kimch321.spring.semiprojectv7.service.JoinService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import java.io.IOException;
 
 @Controller
@@ -25,8 +30,26 @@ public class JoinController {
     }
 
     @GetMapping("/checkme")
-    public String checkme() {
+    public String checkme(Model m) {
+        m.addAttribute("checkme", new Checkme());
+
         return "join/checkme";
+    }
+
+    @PostMapping("/checkme")
+    public String checkmeok(@Valid Checkme checkme,
+                            BindingResult br, HttpSession sess) {
+        // checkme에서 작성한 이름,주민번호를 joinme에 보내는 방법1
+        // "redirect:/join/joinme?name=abc123&jumin1=123456&jumin2=1234567"
+        // checkme에서 작성한 이름,주민번호를 joinme에 보내는 방법2 - session
+        String viewPage = "redirect:/join/joinme";
+
+        if (br.hasErrors())
+            viewPage = "join/checkme";
+        else
+            sess.setAttribute("ckm", checkme);
+
+        return viewPage;
     }
 
     @PostMapping("/joinme")
