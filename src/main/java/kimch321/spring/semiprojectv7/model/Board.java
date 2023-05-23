581e10609edc89699f988d0a7d3b670556bac0d7
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-23 23:09:11 +0900

    validation 1 - 모델 클래스에 유효성 검사 어노테이션 추가

--- src/main/java/kimch321/spring/semiprojectv7/model/Board.java
+++ src/main/java/kimch321/spring/semiprojectv7/model/Board.java
@@ -7,6 +7,7 @@ import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Data
 @Entity
@@ -18,14 +19,16 @@ public class Board {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long bno;
+    @NotBlank(message = "제목은 필수 입력항목입니다!!") // 문자열 길이 0, 공백 문자열 체크
     private String title;
+    @NotBlank(message = "작성자는 필수 입력항목입니다!!")
     private String userid;
 
     @Column(insertable = false, updatable = false)
     private Integer thumbs;
     @Column(insertable = false, updatable = false)
     private Integer views;
-
+    @NotBlank(message = "본문은 필수 입력항목입니다!!")
     private String content;
 
     @CreatedDate
