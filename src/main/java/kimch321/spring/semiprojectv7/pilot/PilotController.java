550cd3a29e6ad2fc79752ed65c523a5b6d6530c3
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-24 11:50:03 +0900

    겹치지 않는 파일명 작성2 - uuid 파일명과 확장자 사이에 UUID 작성(파일럿 프로그램)

--- src/main/java/kimch321/spring/semiprojectv7/pilot/PilotController.java
+++ src/main/java/kimch321/spring/semiprojectv7/pilot/PilotController.java
@@ -30,21 +30,28 @@ public class PilotController {
         // 멀티파트 폼 요소 처리
         if(attach.isEmpty()) m.addAttribute("attach", "첨부파일이 없어요!");
         else {
+            String fname = attach.getOriginalFilename();
             // 업로드한 파일이름 알아내기
-            m.addAttribute("filename", attach.getOriginalFilename());
+            m.addAttribute("filename", fname);
             // 업로드한 파일종류 알아내기
             m.addAttribute("filetype", attach.getContentType());
             // 업로드한 파일크기 알아내기
             m.addAttribute("filesize", attach.getSize()/1024);
             // 겹치지 않는 파일명 작성을 위해 유니크한 값 생성1
-            UUID uuid = UUID.randomUUID();
+            // 파일이름 + uuid + 확장자
+            // abc.jpg => abc375e954c-ec7b-476d-aaf1-71fb31e7c48b.jpg
+            String uuid = UUID.randomUUID().toString().replace("-","");
             m.addAttribute("uuid", uuid);
+
+            // 파일명과 확장자 분리하기
+            String fileName = fname.split("[.]")[0];
+            String fileExt = fname.split("[.]")[1];
 
             // 겹치지 않는 파일명 작성을 위해 유니크한 값 생성2
 
 
             // 업로드한 파일 저장하기
-            attach.transferTo(new File("C:/Java/bootUpload/" + uuid + attach.getOriginalFilename()));
+            attach.transferTo(new File("C:/Java/bootUpload/" + fileName + uuid + "." + fileExt));
         }
 
         return "pilot/list";
