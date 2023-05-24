53ccefae83a4863750aac9f410ef646d9854b82b
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-24 20:08:09 +0900

    pds, pdsattach 모델 작성

---
+++ src/main/java/kimch321/spring/semiprojectv7/model/Pds.java
@@ -0,0 +1,42 @@
+package kimch321.spring.semiprojectv7.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "PDS")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Pds {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pno;
+
+    @NotBlank(message = "제목은 필수항목입니다!!")
+    private String title;
+    @NotBlank(message = "작성자는 필수항목입니다!!")
+    private String userid;
+
+    @Column(insertable = false, updatable = false)
+    private int thumbs;
+    @Column(insertable = false, updatable = false)
+    private int views;
+
+    @NotBlank(message = "본문은 필수항목입니다!!")
+    private String content;
+
+    private String uuid;
+
+    @CreatedDate
+    @Column(insertable = false, updatable = false)
+    private LocalDateTime regdate;
+}
---
+++ src/main/java/kimch321/spring/semiprojectv7/model/PdsAttach.java
@@ -0,0 +1,29 @@
+package kimch321.spring.semiprojectv7.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "PDSATTACH")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PdsAttach {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long pano;
+
+    private String fname;
+    private String ftype;
+    private String fsize;
+
+    @Column(insertable = false, updatable = false)
+    private String fdowns;
+
+    private Integer pno;
+}
