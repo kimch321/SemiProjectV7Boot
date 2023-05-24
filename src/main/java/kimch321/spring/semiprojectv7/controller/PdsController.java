64a669404ab2d6db4a98e19b2fa820d5c9b3203c
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-24 20:10:08 +0900

    자료실 - 컨트롤러 구현

--- src/main/java/kimch321/spring/semiprojectv7/controller/PdsController.java
+++ src/main/java/kimch321/spring/semiprojectv7/controller/PdsController.java
@@ -1,23 +1,44 @@
 package kimch321.spring.semiprojectv7.controller;
 
+import kimch321.spring.semiprojectv7.model.Pds;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/pds")
 public class PdsController {
 
-    @GetMapping("/list")
-    public ModelAndView list(@RequestParam int cpg) {
-        ModelAndView mv = new ModelAndView();
+    @Autowired
+    PdsService pdssrv;
 
-        mv.addObject("cpg",cpg);
-        mv.addObject("stpg",((cpg-1)/10)*10 + 1);
-        mv.setViewName("pds/list");
-        return mv;
+    @GetMapping("/list")
+    public String list() {
+        return "pds/list";
+    }
+
+    @GetMapping("/write")
+    public String write(Model m) {
+        m.addAttribute("pds", new Pds());
+
+        return "pds/write";
+    }
+
+    @PostMapping("/write")
+    public String writeok(Pds pds, MultipartFile attach) {
+        String viewPage = "error";
+
+        int pno = pdssrv.newPds(pds);
+        if (pdssrv.newPdsAttach(attach, pno))
+            viewPage = "redirect:/pds/list";
+
+        return viewPage;
     }
 
 }
