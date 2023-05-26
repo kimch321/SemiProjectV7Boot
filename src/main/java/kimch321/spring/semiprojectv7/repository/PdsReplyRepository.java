f0d7347d55801aa9b7d7673586427acaf6d8afdd
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-26 16:08:39 +0900

    자료실 댓글 - 댓글쓰기 repo 구현

--- src/main/java/kimch321/spring/semiprojectv7/repository/PdsReplyRepository.java
+++ src/main/java/kimch321/spring/semiprojectv7/repository/PdsReplyRepository.java
@@ -2,13 +2,20 @@ package kimch321.spring.semiprojectv7.repository;
 
 import kimch321.spring.semiprojectv7.model.PdsReply;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface PdsReplyRepository extends JpaRepository<PdsReply, Long> {
 
     @Query("from PdsReply where pno = :pno order by refno, regdate asc ")
     List<PdsReply> findByPnoOrderByRefnoAscRegdateAsc(int pno);
+
+    @Transactional
+    @Modifying
+    @Query("update PdsReply set refno = :rpno where rpno = :rpno")
+    void updateRefno(@Param("rpno")int rpno);
 }
