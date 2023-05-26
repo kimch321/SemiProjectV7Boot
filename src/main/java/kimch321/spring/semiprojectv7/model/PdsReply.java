91501c146e606e1b5aab9767a0c3e813041cd9d2
KIM CHUNG IL <cnddlf12@naver.com>
2023-05-26 10:35:52 +0900

    자료실 댓글 - 댓글 모델 작성

---
+++ src/main/java/kimch321/spring/semiprojectv7/model/PdsReply.java
@@ -0,0 +1,34 @@
+package kimch321.spring.semiprojectv7.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "PDSREPLY")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class PdsReply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long rpno;
+
+    @NotBlank(message = "댓글은 필수항목입니다")
+    private String reply;
+    @NotBlank(message = "작성자는 필수항목입니다")
+    private String userid;
+    private String pno;
+    private String refno;
+    @CreatedDate
+    @Column(insertable = false, updatable = false)
+    private LocalDateTime regdate;
+
+}
